bab1a86985ff7a3d10ecc6fbc3aa9b0af3642536
Matt McCormack <matthewmccormack@gmail.com>
2024-04-07 09:25:09 +0100

    Remove home link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +15,6 @@ export default function RootLayout ({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Link href='/'>Home</Link>
         {children}
       </body>
     </html>

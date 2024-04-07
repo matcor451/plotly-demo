2450d26ac836e37c65843d9dde3ce4add271572c
Matt McCormack <matthewmccormack@gmail.com>
2024-04-07 09:22:23 +0100

    remove basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/plotly-demo'
-};
+  output: 'export'
+}
 
-export default nextConfig;
+export default nextConfig
